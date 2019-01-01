12779fda57d32e4951531f1d60eb121db1efba96
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2019-01-01 16:37:47 +0100

    Added BthPS3ConnectionObjectInit

--- BthPS3/Connection.c
+++ BthPS3/Connection.c
@@ -1,1 +1,66 @@
 #include "Driver.h"
+
+_IRQL_requires_max_(DISPATCH_LEVEL)
+NTSTATUS
+BthPS3ConnectionObjectInit(
+    _In_ WDFOBJECT ConnectionObject,
+    _In_ PBTHPS3_DEVICE_CONTEXT_HEADER DevCtxHdr
+)
+{
+    NTSTATUS status;
+    WDF_OBJECT_ATTRIBUTES attributes;
+    PBTHPS3_CONNECTION connection = GetConnectionObjectContext(ConnectionObject);
+
+
+    connection->DevCtxHdr = DevCtxHdr;
+
+    connection->ConnectionState = ConnectionStateInitialized;
+
+    //
+    // Initialize spinlock
+    //
+
+    WDF_OBJECT_ATTRIBUTES_INIT(&attributes);
+    attributes.ParentObject = ConnectionObject;
+
+    status = WdfSpinLockCreate(
+        &attributes,
+        &connection->ConnectionLock
+    );
+    if (!NT_SUCCESS(status))
+    {
+        goto exit;
+    }
+
+    //
+    // Create connect/disconnect request
+    //
+
+    status = WdfRequestCreate(
+        &attributes,
+        DevCtxHdr->IoTarget,
+        &connection->ConnectDisconnectRequest
+    );
+
+    if (!NT_SUCCESS(status))
+    {
+        return status;
+    }
+
+    //
+    // Initialize event
+    //
+
+    KeInitializeEvent(&connection->DisconnectEvent, NotificationEvent, TRUE);
+
+    //
+    // Initalize list entry
+    //
+
+    InitializeListHead(&connection->ConnectionListEntry);
+
+    connection->ConnectionState = ConnectionStateInitialized;
+
+exit:
+    return status;
+}

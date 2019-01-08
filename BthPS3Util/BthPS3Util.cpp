6aa29207f3bae0d3fe703cbe91e77e78452a6518
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2019-01-08 22:16:25 +0100

    Implemented basic functionality

--- BthPS3Util/BthPS3Util.cpp
+++ BthPS3Util/BthPS3Util.cpp
@@ -3,13 +3,133 @@
 
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
+#include <strsafe.h>
 #include <bthsdpdef.h>
 #include <bthdef.h>
 #include <BlueToothApis.h>
 #include <iostream>
+#include <initguid.h>
+#include "Public.h"
 #include "argh.h"
 
-int main()
+
+BOOL
+AdjustProcessPrivileges(
+);
+
+int main(int, char* argv[])
 {
-    std::cout << "Hello World!\n"; 
+    argh::parser cmdl(argv);
+
+    if (FALSE == AdjustProcessPrivileges())
+    {
+        return ERROR_ACCESS_DENIED;
+    }
+
+    DWORD err = ERROR_SUCCESS;
+    BLUETOOTH_LOCAL_SERVICE_INFO SvcInfo = { 0 };
+
+    if (FAILED(StringCbCopyW(SvcInfo.szName, sizeof(SvcInfo.szName), BthPS3ServiceName)))
+    {
+        std::cout << "Couldn't set service name" << std::endl;
+        return GetLastError();
+    }
+
+    if (cmdl[{ "-i", "--install" }])
+    {
+        SvcInfo.Enabled = TRUE;
+
+        if (ERROR_SUCCESS != (err = BluetoothSetLocalServiceInfo(
+            nullptr, //callee would select the first found radio
+            &BTHPS3_SERVICE_GUID,
+            0,
+            &SvcInfo
+        )))
+        {
+            std::cout << "BluetoothSetLocalServiceInfo failed, err " << err << std::endl;
+            return GetLastError();
+        }
+
+        std::cout << "Service installed successfully" << std::endl;
+
+        return ERROR_SUCCESS;
+    }
+
+    if (cmdl[{ "-u", "--uninstall" }])
+    {
+        SvcInfo.Enabled = FALSE;
+
+        if (ERROR_SUCCESS != (err = BluetoothSetLocalServiceInfo(
+            nullptr, //callee would select the first found radio
+            &BTHPS3_SERVICE_GUID,
+            0,
+            &SvcInfo
+        )))
+        {
+            std::cout << "BluetoothSetLocalServiceInfo failed, err " << err << std::endl;
+            return GetLastError();
+        }
+
+        std::cout << "Service uninstalled successfully" << std::endl;
+
+        return ERROR_SUCCESS;
+    }
+
+    std::cout << "usage: .\\BthPS3Util -i|-u" << std::endl;
+
+    return ERROR_INVALID_PARAMETER;
 }
+
+BOOL
+AdjustProcessPrivileges(
+)
+{
+    HANDLE procToken;
+    LUID luid;
+    TOKEN_PRIVILEGES tp;
+    BOOL bRetVal;
+    DWORD err;
+
+    bRetVal = OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &procToken);
+    if (!bRetVal)
+    {
+        err = GetLastError();
+        printf("OpenProcessToken failed, err = %d\n", err);
+        goto exit;
+    }
+
+    bRetVal = LookupPrivilegeValue(NULL, SE_LOAD_DRIVER_NAME, &luid);
+    if (!bRetVal)
+    {
+        err = GetLastError();
+        printf("LookupPrivilegeValue failed, err = %d\n", err);
+        goto exit1;
+    }
+
+    tp.PrivilegeCount = 1;
+    tp.Privileges[0].Luid = luid;
+    tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+
+    //
+    // AdjustTokenPrivileges can succeed even when privileges are not adjusted.
+    // In such case GetLastError returns ERROR_NOT_ALL_ASSIGNED.
+    //
+    // Hence we check for GetLastError in both success and failure case.
+    //
+
+    (void)AdjustTokenPrivileges(procToken, FALSE, &tp, sizeof(TOKEN_PRIVILEGES), (PTOKEN_PRIVILEGES)NULL, (PDWORD)NULL);
+    err = GetLastError();
+
+    if (err != ERROR_SUCCESS)
+    {
+        bRetVal = FALSE;
+        printf("AdjustTokenPrivileges failed, err = %d\n", err);
+        goto exit1;
+    }
+
+exit1:
+    CloseHandle(procToken);
+exit:
+    return bRetVal;
+}
+

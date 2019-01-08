dbf8620158ec43cf5baa1624a3489f7bf99aeeca
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2019-01-08 21:39:25 +0100

    Removed unused code

--- BthPS3/L2CAP.h
+++ BthPS3/L2CAP.h
@@ -1,31 +1,5 @@
 #pragma once
 
-VOID
-FORCEINLINE
-L2CAP_REUSE_OPEN_CHANNEL_RESPONSE(
-    PBTH_PROFILE_DRIVER_INTERFACE pInterface,
-    BRB_TYPE Type,
-    struct _BRB_L2CA_OPEN_CHANNEL *pBrb
-)
-{
-    BTH_ADDR addr;
-    USHORT psm;
-    L2CAP_CHANNEL_HANDLE handle;
-
-    if (!pInterface || !pBrb) {
-        return;
-    }
-
-    addr = pBrb->BtAddress;
-    psm = pBrb->Psm;
-    handle = pBrb->ChannelHandle;
-
-    pInterface->BthReuseBrb((PBRB)pBrb, Type);
-
-    pBrb->BtAddress = addr;
-    pBrb->Psm = psm;
-    pBrb->ChannelHandle = handle;
-}
 
 _IRQL_requires_max_(DISPATCH_LEVEL)
 NTSTATUS

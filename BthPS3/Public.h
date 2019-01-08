6deb120f3ad7c60a8be87c717affd769e6663cde
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2019-01-08 21:38:57 +0100

    Rectified public.h

--- BthPS3/Public.h
+++ BthPS3/Public.h
@@ -1,24 +1,6 @@
-/*++
 
-Module Name:
-
-    public.h
-
-Abstract:
-
-    This module contains the common declarations shared by driver
-    and user applications.
-
-Environment:
-
-    user and kernel
-
---*/
-
-//
-// Define an Interface Guid so that apps can find the device and talk to it.
-//
-
-DEFINE_GUID (GUID_DEVINTERFACE_BthPS3,
+DEFINE_GUID (BTHPS3_SERVICE_GUID,
     0x1cb831ea,0x79cd,0x4508,0xb0,0xfc,0x85,0xf7,0xc8,0x5a,0xe8,0xe0);
 // {1cb831ea-79cd-4508-b0fc-85f7c85ae8e0}
+
+extern __declspec(selectany) const PWSTR BthPS3ServiceName = L"BthPS3Service";

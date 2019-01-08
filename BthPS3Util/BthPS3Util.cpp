24b4ba042f5002ee9322db5f7b0ad72b3edcaef0
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2019-01-08 21:49:10 +0100

    Added BthPS3Util Changed GUID in INF

---
+++ BthPS3Util/BthPS3Util.cpp
@@ -0,0 +1,15 @@
+// BthPS3Util.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <bthsdpdef.h>
+#include <bthdef.h>
+#include <BlueToothApis.h>
+#include <iostream>
+#include "argh.h"
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}

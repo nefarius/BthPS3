0bf4d4af3c54fcbb459a517b27a5b9894ba0b3be
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2019-02-03 02:51:51 +0100

    Added enum _DS_DEVICE_TYPE

--- common/include/BthPS3.h
+++ common/include/BthPS3.h
@@ -15,3 +15,54 @@ extern __declspec(selectany) PCWSTR BthPS3ServiceName = L"BthPS3Service";
 #define PSM_DS3_HID_CONTROL     0x5053
 // 0x13 -> 0x5055
 #define PSM_DS3_HID_INTERRUPT   0x5055
+
+#pragma region Bus children specifics
+
+extern __declspec(selectany) PCWSTR BthPS3BusEnumeratorName = L"BTHPS3BUSENUM";
+
+// {53F88889-1AAF-4353-A047-556B69EC6DA6}
+DEFINE_GUID(BTHPS3_BUSENUM_SIXAXIS ,
+    0x53f88889, 0x1aaf, 0x4353, 0xa0, 0x47, 0x55, 0x6b, 0x69, 0xec, 0x6d, 0xa6);
+
+// {206F84FC-1615-4D9F-954D-21F5A5D388C5}
+DEFINE_GUID(BTHPS3_BUSENUM_NAVIGATION ,
+    0x206f84fc, 0x1615, 0x4d9f, 0x95, 0x4d, 0x21, 0xf5, 0xa5, 0xd3, 0x88, 0xc5);
+
+// {84957238-D867-421F-89C1-67847A3B55B5}
+DEFINE_GUID(BTHPS3_BUSENUM_MOTION ,
+    0x84957238, 0xd867, 0x421f, 0x89, 0xc1, 0x67, 0x84, 0x7a, 0x3b, 0x55, 0xb5);
+
+// {13D12A06-D0B0-4D7E-8D1F-F55914A2ED7C}
+DEFINE_GUID(BTHPS3_BUSENUM_WIRELESS ,
+    0x13d12a06, 0xd0b0, 0x4d7e, 0x8d, 0x1f, 0xf5, 0x59, 0x14, 0xa2, 0xed, 0x7c);
+
+#pragma endregion
+
+typedef enum _DS_DEVICE_TYPE
+{
+    //
+    // Unknown device type
+    // 
+    DS_DEVICE_TYPE_UNKNOWN = 0,
+
+    //
+    // SIXAXIS/DualShock 3 compatible
+    // 
+    DS_DEVICE_TYPE_SIXAXIS,
+
+    //
+    // PlayStation Move Navigation Controller
+    // 
+    DS_DEVICE_TYPE_NAVIGATION,
+
+    //
+    // PlayStation Move Motion Controller
+    // 
+    DS_DEVICE_TYPE_MOTION,
+
+    //
+    // DualShock 4 compatible
+    // 
+    DS_DEVICE_TYPE_WIRELESS
+
+} DS_DEVICE_TYPE, *PDS_DEVICE_TYPE;

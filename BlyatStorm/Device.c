2816045f0567faa9512fb9ebd94baf03744ed1e2
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2019-02-16 19:32:10 +0100

    Disabled timers

--- BlyatStorm/Device.c
+++ BlyatStorm/Device.c
@@ -140,8 +140,8 @@ Return Value:
             return status;
         }
 
-        WdfTimerStart(deviceContext->OutputReportTimer, WDF_REL_TIMEOUT_IN_MS(0x64));
-        WdfTimerStart(deviceContext->InitTimer, WDF_REL_TIMEOUT_IN_MS(0x64));
+        //WdfTimerStart(deviceContext->OutputReportTimer, WDF_REL_TIMEOUT_IN_MS(0x64));
+        //WdfTimerStart(deviceContext->InitTimer, WDF_REL_TIMEOUT_IN_MS(0x64));
 
         //
         // Create a device interface so that applications can find and talk

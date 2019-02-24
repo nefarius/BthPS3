cdc81a149629c3cf057120cc1f76474e524d3ee9
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2019-02-24 19:18:35 +0100

    Set STATUS_INVALID_PARAMETER as default

--- BthPS3/Queue.c
+++ BthPS3/Queue.c
@@ -373,6 +373,8 @@ void BthPS3_EvtWdfIoQueueIoInternalDeviceControl(
             "Unknown IoControlCode received: 0x%X",
             IoControlCode
         );
+
+        status = STATUS_INVALID_PARAMETER;
         break;
     }
 
@@ -383,34 +385,15 @@ void BthPS3_EvtWdfIoQueueIoInternalDeviceControl(
     TraceEvents(TRACE_LEVEL_VERBOSE, TRACE_QUEUE, "%!FUNC! Exit (status: %!STATUS!)", status);
 }
 
+//
+// Gets invoked when the queue operation is stopped and outstanding requests need to be handled
+// 
 VOID
 BthPS3_EvtIoStop(
     _In_ WDFQUEUE Queue,
     _In_ WDFREQUEST Request,
     _In_ ULONG ActionFlags
 )
-/*++
-
-Routine Description:
-
-    This event is invoked for a power-managed queue before the device leaves the working state (D0).
-
-Arguments:
-
-    Queue -  Handle to the framework queue object that is associated with the
-             I/O request.
-
-    Request - Handle to a framework request object.
-
-    ActionFlags - A bitwise OR of one or more WDF_REQUEST_STOP_ACTION_FLAGS-typed flags
-                  that identify the reason that the callback function is being called
-                  and whether the request is cancelable.
-
-Return Value:
-
-    VOID
-
---*/
 {
     BOOLEAN ret;
 
@@ -418,44 +401,6 @@ Return Value:
         TRACE_QUEUE,
         "%!FUNC! Queue 0x%p, Request 0x%p ActionFlags %d",
         Queue, Request, ActionFlags);
-
-    //
-    // In most cases, the EvtIoStop callback function completes, cancels, or postpones
-    // further processing of the I/O request.
-    //
-    // Typically, the driver uses the following rules:
-    //
-    // - If the driver owns the I/O request, it calls WdfRequestUnmarkCancelable
-    //   (if the request is cancelable) and either calls WdfRequestStopAcknowledge
-    //   with a Requeue value of TRUE, or it calls WdfRequestComplete with a
-    //   completion status value of STATUS_SUCCESS or STATUS_CANCELLED.
-    //
-    //   Before it can call these methods safely, the driver must make sure that
-    //   its implementation of EvtIoStop has exclusive access to the request.
-    //
-    //   In order to do that, the driver must synchronize access to the request
-    //   to prevent other threads from manipulating the request concurrently.
-    //   The synchronization method you choose will depend on your driver's design.
-    //
-    //   For example, if the request is held in a shared context, the EvtIoStop callback
-    //   might acquire an internal driver lock, take the request from the shared context,
-    //   and then release the lock. At this point, the EvtIoStop callback owns the request
-    //   and can safely complete or requeue the request.
-    //
-    // - If the driver has forwarded the I/O request to an I/O target, it either calls
-    //   WdfRequestCancelSentRequest to attempt to cancel the request, or it postpones
-    //   further processing of the request and calls WdfRequestStopAcknowledge with
-    //   a Requeue value of FALSE.
-    //
-    // A driver might choose to take no action in EvtIoStop for requests that are
-    // guaranteed to complete in a small amount of time.
-    //
-    // In this case, the framework waits until the specified request is complete
-    // before moving the device (or system) to a lower power state or removing the device.
-    // Potentially, this inaction can prevent a system from entering its hibernation state
-    // or another low system power state. In extreme cases, it can cause the system
-    // to crash with bugcheck code 9F.
-    //
 
     ret = WdfRequestCancelSentRequest(Request);
 

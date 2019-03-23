05278d21584c5b11a04426289bc9c2d984f4c82e
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2019-03-23 13:22:30 +0100

    Code clean-up

--- common/include/BthPS3.h
+++ common/include/BthPS3.h
@@ -39,68 +39,79 @@ extern __declspec(selectany) PCWSTR BthPS3ServiceName = L"BthPS3Service";
 // 0x13 -> 0x5055
 #define PSM_DS3_HID_INTERRUPT   0x5055
 
-#pragma region Bus children specifics
-
+//
+// Bus enumerator name
+// 
 extern __declspec(selectany) PCWSTR BthPS3BusEnumeratorName = L"BTHPS3BUS";
+
+#pragma region PDO Hardware IDs
 
 // SIXAXIS/DualShock Hardware ID
 DEFINE_GUID(GUID_BUSENUM_BTHPS3_SIXAXIS,
     0x53f88889, 0x1aaf, 0x4353, 0xa0, 0x47, 0x55, 0x6b, 0x69, 0xec, 0x6d, 0xa6);
 // {53F88889-1AAF-4353-A047-556B69EC6DA6}
 
-// SIXAXIS/DualShock Device Class GUID
-DEFINE_GUID(GUID_DEVCLASS_BTHPS3_SIXAXIS,
-    0x2ffad411, 0x8a38, 0x4a36, 0x95, 0x7a, 0xc2, 0xe2, 0xd7, 0x69, 0xbe, 0x62);
-// {2FFAD411-8A38-4A36-957A-C2E2D769BE62}
-
-// SIXAXIS/DualShock Device Interface GUID
-DEFINE_GUID(GUID_DEVINTERFACE_BTHPS3_SIXAXIS, 
-0x7b0eae3d, 0x4414, 0x4024, 0xbc, 0xbd, 0x1c, 0x21, 0x52, 0x37, 0x68, 0xce);
-// {7B0EAE3D-4414-4024-BCBD-1C21523768CE}
-
 // Navigation Hardware ID
 DEFINE_GUID(GUID_BUSENUM_BTHPS3_NAVIGATION,
     0x206f84fc, 0x1615, 0x4d9f, 0x95, 0x4d, 0x21, 0xf5, 0xa5, 0xd3, 0x88, 0xc5);
 // {206F84FC-1615-4D9F-954D-21F5A5D388C5}
-
-// Navigation Device Class GUID
-DEFINE_GUID(GUID_DEVCLASS_BTHPS3_NAVIGATION,
-    0x2ffad411, 0x8a38, 0x4a36, 0x95, 0x7a, 0xc2, 0xe2, 0xd7, 0x69, 0xbe, 0x62);
-// {2FFAD411-8A38-4A36-957A-C2E2D769BE62}
-
-// Navigation Device Interface GUID
-DEFINE_GUID(GUID_DEVINTERFACE_BTHPS3_NAVIGATION, 
-0x3e53723a, 0x440c, 0x40af, 0x88, 0x95, 0xea, 0x43, 0x9d, 0x75, 0xe7, 0xbe);
-// {3E53723A-440C-40AF-8895-EA439D75E7BE}
 
 // Motion Hardware ID
 DEFINE_GUID(GUID_BUSENUM_BTHPS3_MOTION,
     0x84957238, 0xd867, 0x421f, 0x89, 0xc1, 0x67, 0x84, 0x7a, 0x3b, 0x55, 0xb5);
 // {84957238-D867-421F-89C1-67847A3B55B5}
 
-// Motion Device Class GUID
-DEFINE_GUID(GUID_DEVCLASS_BTHPS3_MOTION,
-    0x2ffad411, 0x8a38, 0x4a36, 0x95, 0x7a, 0xc2, 0xe2, 0xd7, 0x69, 0xbe, 0x62);
-// {2FFAD411-8A38-4A36-957A-C2E2D769BE62}
-
-// Motion Device Interface GUID
-DEFINE_GUID(GUID_DEVINTERFACE_BTHPS3_MOTION, 
-0xbcec605d, 0x233c, 0x4bef, 0x9a, 0x10, 0xf2, 0xb8, 0x1b, 0x52, 0x97, 0xf6);
-// {BCEC605D-233C-4BEF-9A10-F2B81B5297F6}
-
 // DualShock 4 Rev.1/2 Hardware ID
 DEFINE_GUID(GUID_BUSENUM_BTHPS3_WIRELESS,
     0x13d12a06, 0xd0b0, 0x4d7e, 0x8d, 0x1f, 0xf5, 0x59, 0x14, 0xa2, 0xed, 0x7c);
 // {13D12A06-D0B0-4D7E-8D1F-F55914A2ED7C}
+
+#pragma endregion
+
+#pragma region Raw PDO Device Class GUIDs
+
+// SIXAXIS/DualShock Device Class GUID
+DEFINE_GUID(GUID_DEVCLASS_BTHPS3_SIXAXIS,
+    0x2ffad411, 0x8a38, 0x4a36, 0x95, 0x7a, 0xc2, 0xe2, 0xd7, 0x69, 0xbe, 0x62);
+// {2FFAD411-8A38-4A36-957A-C2E2D769BE62}
+
+// Navigation Device Class GUID
+DEFINE_GUID(GUID_DEVCLASS_BTHPS3_NAVIGATION,
+    0x2ffad411, 0x8a38, 0x4a36, 0x95, 0x7a, 0xc2, 0xe2, 0xd7, 0x69, 0xbe, 0x62);
+// {2FFAD411-8A38-4A36-957A-C2E2D769BE62}
+
+// Motion Device Class GUID
+DEFINE_GUID(GUID_DEVCLASS_BTHPS3_MOTION,
+    0x2ffad411, 0x8a38, 0x4a36, 0x95, 0x7a, 0xc2, 0xe2, 0xd7, 0x69, 0xbe, 0x62);
+// {2FFAD411-8A38-4A36-957A-C2E2D769BE62}
 
 // DualShock 4 Rev.1/2 Device Class GUID
 DEFINE_GUID(GUID_DEVCLASS_BTHPS3_WIRELESS,
     0x2ffad411, 0x8a38, 0x4a36, 0x95, 0x7a, 0xc2, 0xe2, 0xd7, 0x69, 0xbe, 0x62);
 // {2FFAD411-8A38-4A36-957A-C2E2D769BE62}
 
+#pragma endregion
+
+#pragma region PDO Device Interface GUIDs
+
+// SIXAXIS/DualShock Device Interface GUID
+DEFINE_GUID(GUID_DEVINTERFACE_BTHPS3_SIXAXIS,
+    0x7b0eae3d, 0x4414, 0x4024, 0xbc, 0xbd, 0x1c, 0x21, 0x52, 0x37, 0x68, 0xce);
+// {7B0EAE3D-4414-4024-BCBD-1C21523768CE}
+
+// Navigation Device Interface GUID
+DEFINE_GUID(GUID_DEVINTERFACE_BTHPS3_NAVIGATION,
+    0x3e53723a, 0x440c, 0x40af, 0x88, 0x95, 0xea, 0x43, 0x9d, 0x75, 0xe7, 0xbe);
+// {3E53723A-440C-40AF-8895-EA439D75E7BE}
+
+// Motion Device Interface GUID
+DEFINE_GUID(GUID_DEVINTERFACE_BTHPS3_MOTION,
+    0xbcec605d, 0x233c, 0x4bef, 0x9a, 0x10, 0xf2, 0xb8, 0x1b, 0x52, 0x97, 0xf6);
+// {BCEC605D-233C-4BEF-9A10-F2B81B5297F6}
+
 // DualShock 4 Rev.1/2 Device Interface GUID
-DEFINE_GUID(GUID_DEVINTERFACE_BTHPS3_WIRELESS, 
-0x64cb1ee2, 0xb428, 0x4ce8, 0x87, 0x94, 0xf6, 0x80, 0x36, 0xe5, 0x7b, 0xe5);
+DEFINE_GUID(GUID_DEVINTERFACE_BTHPS3_WIRELESS,
+    0x64cb1ee2, 0xb428, 0x4ce8, 0x87, 0x94, 0xf6, 0x80, 0x36, 0xe5, 0x7b, 0xe5);
 // {64CB1EE2-B428-4CE8-8794-F68036E57BE5}
 
 #pragma endregion
@@ -134,7 +145,6 @@ typedef enum _DS_DEVICE_TYPE
     // DualShock 4 compatible
     // 
     DS_DEVICE_TYPE_WIRELESS
-
 } DS_DEVICE_TYPE, *PDS_DEVICE_TYPE;
 
 #define BTHPS3_SIXAXIS_HID_INPUT_REPORT_SIZE        0x32

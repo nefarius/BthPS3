5975bc2ccc5d9dbf98841d8789d9a0ba941e7eba
Benjamin Höglinger-Stelzer <benjamin.hoeglinger@nefarius.at>
2019-09-22 20:22:49 +0200

    Setup installation and removal basically works

---
+++ Setup/install.cmd
@@ -0,0 +1,20 @@
+@echo off
+@setlocal
+
+set MYDIR=%~dp0
+pushd "%MYDIR%"
+
+BthPS3Util.exe --enable-service
+
+BthPS3Util.exe --install-driver --inf-path ".\BthPS3\BthPS3.inf" --force
+
+BthPS3Util.exe --install-driver --inf-path ".\BthPS3PSM\BthPS3PSM_Filter.inf" --force
+
+BthPS3Util.exe --enable-filter
+
+BthPS3Util.exe --restart-host-device
+
+popd
+endlocal
+
+timeout /t 5 /nobreak
---
+++ Setup/uninstall.cmd
@@ -0,0 +1,16 @@
+@echo off
+@setlocal
+
+set MYDIR=%~dp0
+pushd "%MYDIR%"
+
+BthPS3Util.exe --disable-service
+
+BthPS3Util.exe --disable-filter
+
+BthPS3Util.exe --restart-host-device
+
+popd
+endlocal
+
+timeout /t 5 /nobreak

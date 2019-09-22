74a83be50cf1c2930f2f2cb761f0dde47fbc6c16
Benjamin Höglinger-Stelzer <benjamin.hoeglinger@nefarius.at>
2019-09-22 15:56:39 +0200

    Implemented devcon::enable_disable_by_compatible_id Added custom actions to setup

--- BthPS3Util/Devcon.cpp
+++ BthPS3Util/Devcon.cpp
@@ -3,6 +3,7 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 #include <SetupAPI.h>
+#include <tchar.h>
 
 bool devcon::create(std::wstring className, const GUID *classGuid, std::wstring hardwareId)
 {
@@ -35,7 +36,7 @@ bool devcon::create(std::wstring className, const GUID *classGuid, std::wstring 
         &deviceInfoData,
         SPDRP_HARDWAREID,
         (const PBYTE)hardwareId.c_str(),
-        hardwareId.size() * sizeof(WCHAR)
+        (DWORD)(hardwareId.size() * sizeof(WCHAR))
     );
 
     if (!sdrpRet)
@@ -59,4 +60,96 @@ bool devcon::create(std::wstring className, const GUID *classGuid, std::wstring 
     SetupDiDestroyDeviceInfoList(deviceInfoSet);
 
     return true;
+}
+
+bool devcon::enable_disable_by_compatible_id(std::wstring compatibleId, bool state)
+{
+	DWORD i, err;
+	bool found = false, succeeded = false;
+
+	HDEVINFO hDevInfo;
+	SP_DEVINFO_DATA spDevInfoData;
+
+	hDevInfo = SetupDiGetClassDevs(NULL, 0, NULL, DIGCF_ALLCLASSES | DIGCF_PRESENT);
+	if (hDevInfo == INVALID_HANDLE_VALUE)
+	{
+		return succeeded;
+	}
+
+	spDevInfoData.cbSize = sizeof(SP_DEVINFO_DATA);
+	for (i = 0; SetupDiEnumDeviceInfo(hDevInfo, i, &spDevInfoData); i++)
+	{
+		DWORD DataT;
+		LPTSTR p, buffer = NULL;
+		DWORD buffersize = 0;
+
+		// get all devices info
+		while (!SetupDiGetDeviceRegistryProperty(hDevInfo,
+			&spDevInfoData,
+			SPDRP_COMPATIBLEIDS,
+			&DataT,
+			(PBYTE)buffer,
+			buffersize,
+			&buffersize))
+		{
+			if (GetLastError() == ERROR_INVALID_DATA) {
+				break;
+			}
+			else if (GetLastError() == ERROR_INSUFFICIENT_BUFFER) {
+				if (buffer)
+					LocalFree(buffer);
+				buffer = (wchar_t*)LocalAlloc(LPTR, buffersize);
+			}
+			else {
+				goto cleanup_DeviceInfo;
+			}
+		}
+
+		if (GetLastError() == ERROR_INVALID_DATA)
+			continue;
+
+		//find device with CompatibleId
+		for (p = buffer; *p && (p < &buffer[buffersize]); p += lstrlen(p) + sizeof(TCHAR)) {
+			if (!_tcscmp(compatibleId.c_str(), p)) {
+				found = true;
+				break;
+			}
+		}
+
+		if (buffer)
+			LocalFree(buffer);
+
+		// if device found change it's state
+		if (found)
+		{
+			SP_PROPCHANGE_PARAMS params;
+
+			params.ClassInstallHeader.cbSize = sizeof(SP_CLASSINSTALL_HEADER);
+			params.ClassInstallHeader.InstallFunction = DIF_PROPERTYCHANGE;
+			params.Scope = DICS_FLAG_GLOBAL;
+			params.StateChange = (state) ? DICS_ENABLE : DICS_DISABLE;
+
+			// setup proper parameters            
+			if (!SetupDiSetClassInstallParams(hDevInfo, &spDevInfoData, &params.ClassInstallHeader, sizeof(params))) {
+				err = GetLastError();
+			}
+			else {
+				// use parameters
+				if (!SetupDiCallClassInstaller(DIF_PROPERTYCHANGE, hDevInfo, &spDevInfoData)) {
+					err = GetLastError(); // error here  
+				}
+				else { succeeded = true; }
+			}
+
+			break;
+		}
+
+	}
+
+cleanup_DeviceInfo:
+	err = GetLastError();
+	SetupDiDestroyDeviceInfoList(hDevInfo);
+	SetLastError(err);
+
+	return succeeded;
 }
--- BthPS3Util/Devcon.h
+++ BthPS3Util/Devcon.h
@@ -6,4 +6,6 @@
 namespace devcon
 {
     bool create(std::wstring className, const GUID *classGuid, std::wstring hardwareId);
+
+	bool enable_disable_by_compatible_id(std::wstring compatibleId, bool state);
 };

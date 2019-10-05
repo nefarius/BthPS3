58b24869f4269412181e4d97b0593dcdd5213426
Benjamin Höglinger-Stelzer <benjamin.hoeglinger@nefarius.at>
2019-10-05 15:33:55 +0200

    Added more logging to setup

--- Setup/uninstall.cmd
+++ Setup/uninstall.cmd
@@ -4,11 +4,11 @@
 set MYDIR=%~dp0
 pushd "%MYDIR%"
 
-BthPS3Util.exe --disable-service
+BthPS3Util.exe --disable-service >> uninstall.log 2>&1
 
-BthPS3Util.exe --disable-filter
+BthPS3Util.exe --disable-filter >> uninstall.log 2>&1
 
-BthPS3Util.exe --restart-host-device
+BthPS3Util.exe --restart-host-device >> uninstall.log 2>&1
 
 popd
 endlocal

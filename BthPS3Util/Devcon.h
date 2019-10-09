2455c77b7ca4570da9b00a433d6679d6ca2568d5
Benjamin Höglinger-Stelzer <benjamin.hoeglinger@nefarius.at>
2019-10-09 21:41:38 +0200

    Removed deprecated function

--- BthPS3Util/Devcon.h
+++ BthPS3Util/Devcon.h
@@ -7,7 +7,5 @@ namespace devcon
 {
     bool create(std::wstring className, const GUID *classGuid, std::wstring hardwareId);
 
-	bool enable_disable_by_compatible_id(std::wstring compatibleId, bool state);
-
     bool enable_disable_bth_usb_device(bool state);
 };

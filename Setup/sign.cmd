ce4cf694b9c0ca394a78389a7afa8ac43d7d39ec
Benjamin Höglinger-Stelzer <benjamin.hoeglinger@nefarius.at>
2019-10-29 21:59:43 +0100

    Fixed signing

--- Setup/sign.cmd
+++ Setup/sign.cmd
@@ -3,6 +3,6 @@
 
 makecab /f .\BthPS3.ddf
 
-signtool sign /v /ac "C:\Program Files (x86)\Windows Kits\10\CrossCertificates\DigiCert_High_Assurance_EV_Root_CA.crt" /n "Nefarius Software Solutions e.U." /tr http://timestamp.digicert.com /fd sha256 /td sha256 *.cab
+signtool sign /v /ac "C:\Program Files (x86)\Windows Kits\10\CrossCertificates\DigiCert_High_Assurance_EV_Root_CA.crt" /n "Nefarius Software Solutions e.U." /tr http://timestamp.digicert.com /fd sha256 /td sha256 .\disk1\*.cab
 
 endlocal

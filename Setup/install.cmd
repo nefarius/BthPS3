f396afb8dba57f4486600fa90d62c2ba5af59113
Benjamin Höglinger-Stelzer <benjamin.hoeglinger@nefarius.at>
2019-10-30 21:13:30 +0100

    Fixed installation order in install.cmd

--- Setup/install.cmd
+++ Setup/install.cmd
@@ -6,16 +6,22 @@ pushd "%MYDIR%"
 
 type NUL > install.log
 
-BthPS3Util.exe --enable-service >> install.log 2>&1
+rem Create "BthPS3PSM" driver service
+BthPS3Util.exe --create-filter-service --bin-path ".\BthPS3PSM.sys" >> install.log 2>&1
 
-BthPS3Util.exe --install-driver --inf-path ".\BthPS3.inf" --force >> install.log 2>&1
-
-BthPS3Util.exe --install-driver --inf-path ".\BthPS3PSM_Filter.inf" --force >> install.log 2>&1
-
+rem Enable lower filter for BTHUSB
 BthPS3Util.exe --enable-filter >> install.log 2>&1
 
+rem Restart host device (causes BthPS3PSM to load)
 BthPS3Util.exe --restart-host-device >> install.log 2>&1
 
+rem Enable profile service (L2CAP server)
+BthPS3Util.exe --enable-service >> install.log 2>&1
+
+rem Install profile/bus driver
+BthPS3Util.exe --install-driver --inf-path ".\BthPS3.inf" --force >> install.log 2>&1
+
+rem Instruct filter to enable patch
 BthPS3Util.exe --enable-psm-patch --device-index 0 >> install.log 2>&1
 
 popd

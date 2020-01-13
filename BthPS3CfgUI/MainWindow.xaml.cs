ee1b4ff8c7b7b95afe4a354d669c11226e42c08b
Benjamin Höglinger-Stelzer <benjamin.hoeglinger@nefarius.at>
2020-01-13 21:17:39 +0100

    Implementing ViewModel

--- BthPS3CfgUI/MainWindow.xaml.cs
+++ BthPS3CfgUI/MainWindow.xaml.cs
@@ -1,29 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
+﻿using System.Diagnostics;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using AdonisUI.Controls;
 
 namespace BthPS3CfgUI
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : AdonisUI.Controls.AdonisWindow
+    public partial class MainWindow : AdonisWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = new ProfileDriverSettingsViewModel();
         }
 
         private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
---
+++ BthPS3CfgUI/ProfileDriverSettingsViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using BthPS3CfgUI.Annotations;
+using Microsoft.Win32;
+
+namespace BthPS3CfgUI
+{
+    public class ProfileDriverSettingsViewModel : INotifyPropertyChanged
+    {
+        private RegistryKey _bthPs3ServiceParameters;
+
+        public ProfileDriverSettingsViewModel()
+        {
+            _bthPs3ServiceParameters =
+                Registry.LocalMachine.OpenSubKey(
+                    "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\BthPS3\\Parameters", true);
+        }
+
+        public bool IsSIXAXISSupported
+        {
+            get => bool.Parse(_bthPs3ServiceParameters.GetValue("IsSIXAXISSupported", true).ToString());
+            set => _bthPs3ServiceParameters.SetValue("IsSIXAXISSupported", value, RegistryValueKind.DWord);
+        }
+
+        public bool IsNAVIGATIONSupported
+        {
+            get => bool.Parse(_bthPs3ServiceParameters.GetValue("IsNAVIGATIONSupported", true).ToString());
+            set => _bthPs3ServiceParameters.SetValue("IsNAVIGATIONSupported", value, RegistryValueKind.DWord);
+        }
+
+        public bool IsMOTIONSupported
+        {
+            get => bool.Parse(_bthPs3ServiceParameters.GetValue("IsMOTIONSupported", true).ToString());
+            set => _bthPs3ServiceParameters.SetValue("IsMOTIONSupported", value, RegistryValueKind.DWord);
+        }
+
+        public bool IsWIRELESSSupported
+        {
+            get => bool.Parse(_bthPs3ServiceParameters.GetValue("IsWIRELESSSupported", true).ToString());
+            set => _bthPs3ServiceParameters.SetValue("IsWIRELESSSupported", value, RegistryValueKind.DWord);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

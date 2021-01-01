ded66892e7bcdb947c1a5951cd0318e10d01b642
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-01-01 16:53:06 +0100

    Porting code required by CLI utility and new custom actions for installation and removal

---
+++ BthPS3SetupHelper/BthPS3Setup.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "framework.h"
+
+EXTERN_C IMAGE_DOS_HEADER __ImageBase;
+
+namespace winapi
+{
+    BOOL AdjustProcessPrivileges();
+
+    BOOL CreateDriverService(PCSTR ServiceName, PCSTR DisplayName, PCSTR BinaryPath);
+
+    BOOL DeleteDriverService(PCSTR ServiceName);
+
+    std::string GetLastErrorStdStr();
+
+    std::string GetVersionFromFile(std::string FilePath);
+
+    std::string GetImageBasePath();
+};
--- BthPS3Util/BthPS3Util.h
+++ BthPS3Util/BthPS3Util.h
@@ -51,19 +51,4 @@
 
 #include "Devcon.h"
 
-EXTERN_C IMAGE_DOS_HEADER __ImageBase;
-
-namespace winapi
-{
-    BOOL AdjustProcessPrivileges();
-
-    BOOL CreateDriverService(PCSTR ServiceName, PCSTR DisplayName, PCSTR BinaryPath);
-
-    BOOL DeleteDriverService(PCSTR ServiceName);
-
-    std::string GetLastErrorStdStr();
-
-    std::string GetVersionFromFile(std::string FilePath);
-
-    std::string GetImageBasePath();
-};
+#include <BthPS3Setup.h>

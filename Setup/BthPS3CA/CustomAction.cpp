bd0d293e4d372dcb4cd38bf1bcf5bed4d90dd70a
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-01-01 17:03:01 +0100

    Added InstallDrivers and UninstallDrivers function stubs

--- Setup/BthPS3CA/CustomAction.cpp
+++ Setup/BthPS3CA/CustomAction.cpp
@@ -36,6 +36,44 @@ LExit:
 	return WcaFinalize(er);
 }
 
+UINT __stdcall InstallDrivers(
+	MSIHANDLE hInstall
+	)
+{
+	HRESULT hr = S_OK;
+	UINT er = ERROR_SUCCESS;
+
+	hr = WcaInitialize(hInstall, "InstallDrivers");
+	ExitOnFailure(hr, "Failed to initialize");
+
+	WcaLog(LOGMSG_STANDARD, "Initialized.");
+
+	
+		
+LExit:
+	er = SUCCEEDED(hr) ? ERROR_SUCCESS : ERROR_INSTALL_FAILURE;
+	return WcaFinalize(er);
+}
+
+UINT __stdcall UninstallDrivers(
+	MSIHANDLE hInstall
+	)
+{
+	HRESULT hr = S_OK;
+	UINT er = ERROR_SUCCESS;
+
+	hr = WcaInitialize(hInstall, "UninstallDrivers");
+	ExitOnFailure(hr, "Failed to initialize");
+
+	WcaLog(LOGMSG_STANDARD, "Initialized.");
+
+	
+	
+LExit:
+	er = SUCCEEDED(hr) ? ERROR_SUCCESS : ERROR_INSTALL_FAILURE;
+	return WcaFinalize(er);
+}
+
 
 // DllMain - Initialize and cleanup WiX custom action utils.
 extern "C" BOOL WINAPI DllMain(

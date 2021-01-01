b5fb00007adbe9e7779c1462e707bf26fb16d6bb
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-01-01 16:32:25 +0100

    Added BthPS3SetupHelper project to put shared code in

---
+++ BthPS3SetupHelper/BthPS3SetupHelper.cpp
@@ -0,0 +1,9 @@
+// BthPS3SetupHelper.cpp : Defines the functions for the static library.
+//
+
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnBthPS3SetupHelper()
+{
+}
---
+++ BthPS3SetupHelper/framework.h
@@ -0,0 +1,7 @@
+// header.h : include file for standard system include files,
+// or project specific include files
+//
+
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers

75292da8fb773d69fbc92e3543b5439386d3f0bc
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-01-02 23:09:57 +0100

    Fixed processor architecture

--- Setup/BthPS3CA/CustomAction.cpp
+++ Setup/BthPS3CA/CustomAction.cpp
@@ -1,5 +1,15 @@
 #include "stdafx.h"
 
+#include <pathcch.h>
+
+#include <string>
+#include <Devcon.h>
+#include <BthPS3Setup.h>
+
+const std::wstring g_nullInf = L"BthPS3_PDO_NULL_Device.inf";
+const std::wstring g_profileInf = L"BthPS3.inf";
+const std::wstring g_filterInf = L"BthPS3PSM.inf";
+
 
 UINT __stdcall CheckHostRadioPresence(
 	MSIHANDLE hInstall
@@ -43,18 +53,36 @@ UINT __stdcall InstallDrivers(
 	HRESULT hr = S_OK;
 	UINT er = ERROR_SUCCESS;
 
+	std::wstring nullInfPath(PATHCCH_MAX_CCH, L'\0');
+	std::wstring profileInfPath(PATHCCH_MAX_CCH, L'\0');
+	std::wstring filterInfPath(PATHCCH_MAX_CCH, L'\0');
+	
 	hr = WcaInitialize(hInstall, "InstallDrivers");
 	ExitOnFailure(hr, "Failed to initialize");
 
 	WcaLog(LOGMSG_STANDARD, "Initialized.");
 
-	WCHAR targetPath[MAX_PATH * sizeof(WCHAR)];
+	bool rebootRequired = false;
+	WCHAR targetPath[PATHCCH_MAX_CCH * sizeof(WCHAR)];
 	DWORD length = ARRAYSIZE(targetPath);
 
 	(void)MsiGetProperty(hInstall, L"CustomActionData", targetPath, &length);
+	
+	PathCchCombine(&nullInfPath[0], nullInfPath.size(), targetPath, g_nullInf.c_str());
+	PathCchCombine(&profileInfPath[0], profileInfPath.size(), targetPath, g_profileInf.c_str());
+	PathCchCombine(&filterInfPath[0], filterInfPath.size(), targetPath, g_filterInf.c_str());
 
-	MessageBox(NULL, targetPath, L"Path", MB_OK);
-		
+	do
+	{
+		WcaLog(LOGMSG_STANDARD, "Installing PDO NULL driver.");
+		if (!devcon::install_driver(nullInfPath, &rebootRequired))
+		{
+			WcaLog(LOGMSG_STANDARD, "Failed to install PDO NULL driver, error: %s",
+			       winapi::GetLastErrorStdStr().c_str());
+		}
+	}
+	while (FALSE);
+	
 LExit:
 	er = SUCCEEDED(hr) ? ERROR_SUCCESS : ERROR_INSTALL_FAILURE;
 	return WcaFinalize(er);

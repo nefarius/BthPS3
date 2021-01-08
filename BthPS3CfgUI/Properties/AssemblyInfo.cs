61290faeac2f6d84f0eb19593624c94bafe81a77
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-01-09 00:10:44 +0100

    Added missing meta data in AssemblyInfo.cs Removed obsolete file

--- BthPS3CfgUI/Properties/AssemblyInfo.cs
+++ BthPS3CfgUI/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BthPS3CfgUI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("BthPS3 Driver Configuration Tool")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Nefarius Software Solutions e.U.")]
 [assembly: AssemblyProduct("BthPS3CfgUI")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © Nefarius Software Solutions e.U. 2020-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -53,3 +53,4 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]

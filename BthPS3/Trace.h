9e4c2ab3d62c876b0034912e9630583a69c9ae84
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-01-28 19:48:29 +0100

    Added new tracing helper functions

--- BthPS3/Trace.h
+++ BthPS3/Trace.h
@@ -84,8 +84,28 @@
 // This comment block is scanned by the trace preprocessor to define our
 // Trace function.
 //
-// begin_wpp config
-// FUNC Trace{FLAGS=MYDRIVER_ALL_INFO}(LEVEL, MSG, ...);
-// FUNC TraceEvents(LEVEL, FLAGS, MSG, ...);
-// end_wpp
+// USEPREFIX and USESUFFIX strip all trailing whitespace, so we need to surround
+// FuncExit messages with brackets 
 //
+// begin_wpp config
+// FUNC Trace{FLAG=MYDRIVER_ALL_INFO}(LEVEL, MSG, ...);
+// FUNC TraceEvents(LEVEL, FLAGS, MSG, ...);
+// FUNC FuncEntry{LEVEL=TRACE_LEVEL_VERBOSE}(FLAGS);
+// FUNC FuncEntryArguments{LEVEL=TRACE_LEVEL_VERBOSE}(FLAGS, MSG, ...);
+// FUNC FuncExit{LEVEL=TRACE_LEVEL_VERBOSE}(FLAGS, MSG, ...);
+// FUNC FuncExitVoid{LEVEL=TRACE_LEVEL_VERBOSE}(FLAGS);
+// FUNC TraceError{LEVEL=TRACE_LEVEL_ERROR}(FLAGS, MSG, ...);
+// FUNC TraceInformation{LEVEL=TRACE_LEVEL_INFORMATION}(FLAGS, MSG, ...);
+// FUNC TraceVerbose{LEVEL=TRACE_LEVEL_VERBOSE}(FLAGS, MSG, ...);
+// FUNC FuncExitNoReturn{LEVEL=TRACE_LEVEL_VERBOSE}(FLAGS);
+// USEPREFIX(FuncEntry, "%!STDPREFIX! [%!FUNC!] --> Entry");
+// USEPREFIX(FuncEntryArguments, "%!STDPREFIX! [%!FUNC!] --> Entry <");
+// USEPREFIX(FuncExit, "%!STDPREFIX! [%!FUNC!] <-- Exit <");
+// USESUFFIX(FuncExit, ">");
+// USEPREFIX(FuncExitVoid, "%!STDPREFIX! [%!FUNC!] <-- Exit");
+// USEPREFIX(TraceError, "%!STDPREFIX! [%!FUNC!] ERROR:");
+// USEPREFIX(TraceEvents, "%!STDPREFIX! [%!FUNC!] ");
+// USEPREFIX(TraceInformation, "%!STDPREFIX! [%!FUNC!] ");
+// USEPREFIX(TraceVerbose, "%!STDPREFIX! [%!FUNC!] ");
+// USEPREFIX(FuncExitNoReturn, "%!STDPREFIX! [%!FUNC!] <--");
+// end_wpp

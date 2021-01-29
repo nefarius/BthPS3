0817c4111f0a35158f5be4d9d98e73fa6a6326d1
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-01-29 19:48:16 +0100

    Added missing include

--- BthPS3SetupHelper/framework.h
+++ BthPS3SetupHelper/framework.h
@@ -7,6 +7,7 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 #include <Windows.h>
 #include <bluetoothapis.h>
+#include <devpropdef.h>
 
 #include <string>
 #include <sstream>

b80c1ce1e08e9efc9f865e5ac5151bcfd1f296bc
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-07-07 20:06:31 +0200

    Updated production binaries

--- Setup/stage1.ps1
+++ Setup/stage1.ps1
@@ -4,10 +4,8 @@ $certName = "Nefarius Software Solutions e.U."
 
 $files =    "`".\drivers\BthPS3_ARM64\*.sys`" " +
             "`".\drivers\BthPS3_x64\*.sys`" " +
-            "`".\drivers\BthPS3_x86\*.sys`" " +
 			"`".\drivers\BthPS3PSM_ARM64\*.sys`" " +
-            "`".\drivers\BthPS3PSM_x64\*.sys`" " +
-            "`".\drivers\BthPS3PSM_x86\*.sys`" "
+            "`".\drivers\BthPS3PSM_x64\*.sys`" "
 
 # sign with adding to existing certificates
 Invoke-Expression "& `"$signTool`" sign /v /as /n `"$certName`" /tr $timestampUrl /fd sha256 /td sha256 $files"

0ae007b21e3f492d3c3e2640192f199031ce6645
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-07-07 20:09:06 +0200

    Update stage2.ps1

--- Setup/stage2.ps1
+++ Setup/stage2.ps1
@@ -4,8 +4,7 @@ $certName = "Nefarius Software Solutions e.U."
 
 # List of files to sign
 $files =    "`"..\bin\ARM64\*.msi`" " + 
-            "`"..\bin\x64\*.msi`" " + 
-            "`"..\bin\x86\*.msi`" "
+            "`"..\bin\x64\*.msi`" "
 
 # sign with only one certificate
 Invoke-Expression "& `"$signTool`" sign /v /n `"$certName`" /tr $timestampUrl /fd sha256 /td sha256 $files"

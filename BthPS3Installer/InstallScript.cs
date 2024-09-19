24093a7056baddb2d4669f7871bb60dd064ed97a
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-09-19 19:03:39 +0200

    More logic

--- BthPS3Installer/InstallScript.cs
+++ BthPS3Installer/InstallScript.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Buffers;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing.Drawing2D;
+using System.IO;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -8,6 +12,7 @@ using CliWrap;
 
 using Microsoft.Win32;
 
+using Nefarius.BthPS3.Setup.Util;
 using Nefarius.Utilities.Bluetooth;
 using Nefarius.Utilities.DeviceManagement.PnP;
 
@@ -16,19 +21,48 @@ using WixSharp.Forms;
 
 using WixToolset.Dtf.WindowsInstaller;
 
+using File = WixSharp.File;
+using RegistryHive = WixSharp.RegistryHive;
+
 namespace Nefarius.BthPS3.Setup;
 
 internal class InstallScript
 {
     public const string ProductName = "BthPS3 Bluetooth Drivers";
+    public const string SetupRoot = @"..\setup";
 
     private static void Main()
     {
         Version version = Version.Parse(BuildVariables.SetupVersion);
+        string archShortName = ArchitectureInfo.PlatformShortName;
+
+        string driverPath = Path.Combine(SetupRoot, @"drivers\BthPS3_x64\BthPS3.sys");
+        Version driverVersion = Version.Parse(FileVersionInfo.GetVersionInfo(driverPath).FileVersion);
+
+        string filterPath = Path.Combine(SetupRoot, @"drivers\BthPS3PSM_x64\BthPS3PSM.sys");
+        Version filterVersion = Version.Parse(FileVersionInfo.GetVersionInfo(filterPath).FileVersion);
+
+        Console.WriteLine($"Setup version: {version}");
+        Console.WriteLine($"Driver version: {driverVersion}");
+        Console.WriteLine($"Filter version: {filterVersion}");
+
+        Feature driversFeature = new(ProductName, true, false)
+        {
+            Description = "Installs the Nefarius BthPS3 drivers for PS3 peripherals. " +
+                          "This is a mandatory core component and can't be de-selected."
+        };
 
         ManagedProject project = new(ProductName,
-            new Dir(@"%ProgramFiles%\Nefarius Software Solutions\BthPS3",
-                new File("InstallScript.cs")))
+            new Dir(driversFeature, @"%ProgramFiles%\Nefarius Software Solutions\BthPS3",
+                new File("InstallScript.cs")),
+            // registry values
+            new RegKey(driversFeature, RegistryHive.LocalMachine,
+                $@"Software\Nefarius Software Solutions e.U.\{ProductName}",
+                new RegValue("Path", "[INSTALLDIR]") { Win64 = true },
+                new RegValue("Version", version.ToString()) { Win64 = true },
+                new RegValue("DriverVersion", driverVersion.ToString()) { Win64 = true },
+                new RegValue("FilterVersion", filterVersion.ToString()) { Win64 = true }
+            ) { Win64 = true })
         {
             GUID = new Guid("CC32A6ED-BDFE-4D51-9FFF-2AB51D9ECE18"),
             CAConfigFile = "CustomActions.config",
@@ -102,6 +136,27 @@ internal class InstallScript
         {
             MessageBox.Show(e.ToString(), "AfterExecute");
         }
+    }
+
+    /// <summary>
+    ///     Recursively resolves all subdirectories and their containing files.
+    /// </summary>
+    private static List<Dir> GetSubDirectories(Feature feature, string directory)
+    {
+        List<Dir> subDirectoryInfosCollection = new();
+
+        foreach (string subDirectory in Directory.GetDirectories(directory))
+        {
+            string subDirectoryName = subDirectory.Remove(0, subDirectory.LastIndexOf('\\') + 1);
+            Dir newDir =
+                new(feature, subDirectoryName, new Files(feature, subDirectory + @"\*.*")) { Name = subDirectoryName };
+            subDirectoryInfosCollection.Add(newDir);
+
+            // Recursively traverse nested directories
+            GetSubDirectories(feature, subDirectory);
+        }
+
+        return subDirectoryInfosCollection;
     }
 }
 

f72d2c64f27c22a3fca5e08baf080013cf9d3df0
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-09-19 18:23:07 +0200

    More work on setup

--- BthPS3Installer/InstallScript.cs
+++ BthPS3Installer/InstallScript.cs
@@ -1,5 +1,15 @@
 ﻿using System;
+using System.Buffers;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using CliWrap;
+
+using Microsoft.Win32;
+
+using Nefarius.Utilities.Bluetooth;
+using Nefarius.Utilities.DeviceManagement.PnP;
 
 using WixSharp;
 using WixSharp.Forms;
@@ -8,59 +18,88 @@ namespace Nefarius.BthPS3.Setup
 {
     internal class InstallScript
     {
-        static void Main()
+        public const string ProductName = "BthPS3 Bluetooth Drivers";
+
+        private static void Main()
         {
-            var project = new ManagedProject("MyProduct",
-                             new Dir(@"%ProgramFiles%\My Company\My Product",
-                                 new File("InstallScript.cs")));
+            Version version = Version.Parse(BuildVariables.SetupVersion);
 
-            project.GUID = new Guid("c137bbc6-2f60-4c8a-afcc-204a5f8f9a23");
-
-            project.ManagedUI = ManagedUI.Empty;    //no standard UI dialogs
-            project.ManagedUI = ManagedUI.Default;  //all standard UI dialogs
-
-            //custom set of standard UI dialogs
-            project.ManagedUI = new ManagedUI();
+            ManagedProject project = new ManagedProject(ProductName,
+                new Dir(@"%ProgramFiles%\Nefarius Software Solutions\BthPS3",
+                    new File("InstallScript.cs")))
+            {
+                GUID = new Guid("CC32A6ED-BDFE-4D51-9FFF-2AB51D9ECE18"),
+                CAConfigFile = "CustomActions.config",
+                OutFileName = $"Nefarius_BthPS3_Drivers_x64_arm64_v{version}",
+                //custom set of standard UI dialogs
+                ManagedUI = new ManagedUI(),
+                Version = version,
+                Platform = Platform.x64,
+                WildCardDedup = Project.UniqueFileNameDedup,
+                MajorUpgradeStrategy = MajorUpgradeStrategy.Default
+            };
 
             project.ManagedUI.InstallDialogs.Add(Dialogs.Welcome)
-                                            .Add(Dialogs.Licence)
-                                            .Add(Dialogs.SetupType)
-                                            .Add(Dialogs.Features)
-                                            .Add(Dialogs.InstallDir)
-                                            .Add(Dialogs.Progress)
-                                            .Add(Dialogs.Exit);
+                .Add(Dialogs.Licence)
+                .Add(Dialogs.Features)
+                .Add(Dialogs.Progress)
+                .Add(Dialogs.Exit);
 
             project.ManagedUI.ModifyDialogs.Add(Dialogs.MaintenanceType)
-                                           .Add(Dialogs.Features)
-                                           .Add(Dialogs.Progress)
-                                           .Add(Dialogs.Exit);
+                .Add(Dialogs.Features)
+                .Add(Dialogs.Progress)
+                .Add(Dialogs.Exit);
 
             project.Load += Msi_Load;
             project.BeforeInstall += Msi_BeforeInstall;
             project.AfterInstall += Msi_AfterInstall;
 
-            //project.SourceBaseDir = "<input dir path>";
-            //project.OutDir = "<output dir path>";
+            // embed types of dependencies
+            project.DefaultRefAssemblies.Add(typeof(Devcon).Assembly.Location);
+            project.DefaultRefAssemblies.Add(typeof(HostRadio).Assembly.Location);
+            project.DefaultRefAssemblies.Add(typeof(Cli).Assembly.Location);
+            project.DefaultRefAssemblies.Add(typeof(RegistryKey).Assembly.Location);
+            project.DefaultRefAssemblies.Add(typeof(ValueTask).Assembly.Location);
+            project.DefaultRefAssemblies.Add(typeof(IAsyncDisposable).Assembly.Location);
+            project.DefaultRefAssemblies.Add(typeof(Unsafe).Assembly.Location);
+            project.DefaultRefAssemblies.Add(typeof(BuffersExtensions).Assembly.Location);
+            project.DefaultRefAssemblies.Add(typeof(ArrayPool<>).Assembly.Location);
+
+            project.ControlPanelInfo.ProductIcon = @"..\Setup\Icons\B3.ico";
+            project.ControlPanelInfo.Manufacturer = "Nefarius Software Solutions e.U.";
+            project.ControlPanelInfo.HelpLink = "https://docs.nefarius.at/Community-Support/";
+            project.ControlPanelInfo.UrlInfoAbout = "https://github.com/nefarius/BthPS3";
+            project.ControlPanelInfo.NoModify = true;
+
+            project.MajorUpgradeStrategy.PreventDowngradingVersions.OnlyDetect = false;
+
+            project.ResolveWildCards();
 
             project.BuildMsi();
         }
 
-        static void Msi_Load(SetupEventArgs e)
+        private static void Msi_Load(SetupEventArgs e)
         {
             if (!e.IsUISupressed && !e.IsUninstalling)
+            {
                 MessageBox.Show(e.ToString(), "Load");
+            }
         }
 
-        static void Msi_BeforeInstall(SetupEventArgs e)
+        private static void Msi_BeforeInstall(SetupEventArgs e)
         {
             if (!e.IsUISupressed && !e.IsUninstalling)
+            {
                 MessageBox.Show(e.ToString(), "BeforeInstall");
+            }
         }
 
-        static void Msi_AfterInstall(SetupEventArgs e)
+        private static void Msi_AfterInstall(SetupEventArgs e)
         {
             if (!e.IsUISupressed && !e.IsUninstalling)
+            {
                 MessageBox.Show(e.ToString(), "AfterExecute");
+            }
         }
     }
 }

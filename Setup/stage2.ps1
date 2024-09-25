3ed266da47a3fc7ca6991c4a42d21d620271300a
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-09-25 13:16:11 +0200

    Update stage2.ps1

--- Setup/stage2.ps1
+++ Setup/stage2.ps1
@@ -1,10 +1,13 @@
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]$SetupVersion
+) #end param
+
 $signTool = "$(wdkwhere)\signtool.exe"
 $timestampUrl = "http://timestamp.digicert.com"
 $certName = "Nefarius Software Solutions e.U."
 
-# List of files to sign
-$files =    "`"..\bin\ARM64\*.msi`" " + 
-            "`"..\bin\x64\*.msi`" "
+dotnet build -c:Release -p:SetupVersion=$SetupVersion ..\BthPS3Installer\BthPS3Installer.csproj
 
 # sign with only one certificate
-Invoke-Expression "& `"$signTool`" sign /v /n `"$certName`" /tr $timestampUrl /fd sha256 /td sha256 $files"
+Invoke-Expression "& `"$signTool`" sign /v /n `"$certName`" /tr $timestampUrl /fd sha256 /td sha256 ..\BthPS3Installer\Nefarius_BthPS3_Drivers_x64_arm64_v$SetupVersion.msi"
